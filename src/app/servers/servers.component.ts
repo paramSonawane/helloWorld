6eddc413f52fc58a8e932930815f96d3858291ae
paramSonawane <paramsonawane@gmail.com>
2020-06-17 22:18:02 +0530

    two way binding, removed (input) event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreation: string = "No Server was created";
-  serverName: string = '';
+  serverName: string = 'TestServer';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
