7b6157541182664f3f6f7d383aa81321714029f9
paramSonawane <paramsonawane@gmail.com>
2020-06-17 21:35:28 +0530

    string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,10 @@ import { Component} from '@angular/core';
     templateUrl : './server.component.html' //user backtics `<your html code></>` to add inline html code
 })
 export class serverComponent{
-
+    serverID: number = 10;
+    serverStatus: string = "offline";
+    
+    getServerStatus(){
+    return this.serverStatus;
+    }
 }
