a1ac85858d1fec27c286ad7c514ccc9f085e7e65
paramSonawane <paramsonawane@gmail.com>
2020-06-17 21:04:23 +0530

    mannully created component,explains class selector

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector : '.app-server', //Select by class (check servers html <div class="app-server"></div>)
+    templateUrl : './server.component.html' //user backtics `<your html code></>` to add inline html code
+})
+export class serverComponent{
+
+}
